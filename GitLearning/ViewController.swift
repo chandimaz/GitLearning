289f9242e83c0c40cccdf538fcaf9b5714b2f4c9
Chandima Dasanayaka <chandima.emedia@gmail.com>
2018-03-26 08:21:35 +0530

    changing appearance

---
+++ GitLearning/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitLearning
+//
+//  Created by E_Media on 3/26/18.
+//  Copyright © 2018 E_Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let userName = "Alex"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
